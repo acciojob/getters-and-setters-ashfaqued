920c1403c017a17cdf6391000bba76645cc8cd85
Ashfaque D <87939111+ashfaqued@users.noreply.github.com>
2023-08-03 21:44:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+  get name() {
+    return this._name;
+  }
+  set age(age) {
+    this._age = age;
+  }
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study() {
+    console.log("be.calledWith", `${Student.name} is studying`);
+  }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach() {
+    console.log("be.calledWith", `${Teacher.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
